0f9f9fb8a0d372ebd3edbdc664205d5df783649f
joelcio matias <jmatias@wiley.com>
2021-10-13 06:34:50 -0300

    Created new order method

--- src/app.js
+++ src/app.js
@@ -18,14 +18,35 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //
 const api = require('./integration/apiBinance')
-
+const symbol = process.env.SYMBOL;
 setInterval(async () => {
     // console.log('teste',process.env.CRAWLER_INTERVAL);
+    console.log('Binance api', (await api.exchangeInfo()).serverTime);
     // console.log('Binance api',await api.time());
-    console.log('Binance api',await api.depth('ETHUSDT'));
+    const account = await api.accountInfo()
+    const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1)
+    console.log('POSIÇÃO DA CARTEIRA')
+    console.log(coins)
 
+    // console.log('Binance api',await api.depth(symbol,limit=2));
+    // var x = new Date()
+    // var UTCseconds = (x.getTime());
+    // console.log("UTCseconds", Math.floor(UTCseconds))
+    if(parseInt(coins.find(c => c.asset === 'USDT').free)){
+        // let newOrder = await api.newOrder(symbol,1)
+        let newOrder = await api.newOrder(symbol,1)
+        console.log(newOrder)
+    }
 }, process.env.CRAWLER_INTERVAL)
 
-//
+// console.log(Math.floor((new Date()).getTime() / 1000))
+// console.log(Date.now())
+// console.log((new Date()).getTime())
+// var x = new Date()
+// var UTCseconds = (x.getTime() + x.getTimezoneOffset()*60*1000)/1000;
 
+// console.log("UTCseconds", Math.floor(UTCseconds))
+
+// 1634065114710
+// 1634059654114
 module.exports = app;
--- src/integration/apiBinance.js
+++ src/integration/apiBinance.js
@@ -1,25 +1,67 @@
 const axios = require('axios');
 const querystring = require('querystring');
+const crypto = require('crypto');
+const apiKey = process.env.API_KEY;
+const apiUrl = process.env.API_URL;
+const secretKey = process.env.SECRET_KEY;
+
+async function privateCall(path, data, method = 'GET'){
+    const timestamp = (await exchangeInfo()).serverTime
+    const signature = crypto.createHmac('sha256', secretKey)
+                    .update(`${querystring.stringify({...data, timestamp})}`)
+                    .digest('hex');
+    const newData = {...data, timestamp,signature};
+    const qs = `?${querystring.stringify(newData)}`;
+    let url = `${apiUrl}${path}${qs}`
+    console.log(url)
+    try {
+        const result = await axios({
+            method,
+            url: url,
+            headers: {'X-MBX-APIKEY':apiKey}
+        })
+        return result.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 async function publicCall(path, data, method = 'GET'){
     try {
         const qs = data ? `?${querystring.stringify(data)}` : '';
         const result = await axios({
             method,
-            url: `${process.env.API_URL}${path}${qs}`
+            url: `${apiUrl}${path}${qs}`
         })
         return result.data;
     } catch (error) {
-        console.log(err);
+        console.log(error);
     }
+}
+
+async function accountInfo(){
+    return privateCall('/v3/account')
 }
 
 async function time(){
     return publicCall('/v3/time')
 }
 
+async function newOrder(symbol, quantity, price, side='BUY', type='MARKET'){
+    const data = {symbol, quantity, side, type}
+    if(price) data.price = price;
+    if(type === 'LIMIT') data.timeInforce = 'GTC';
+
+    return privateCall('/v3/order',data,'POST')
+
+}
+
 async function depth(symbol='BTCUSDT', limit=5){
     return publicCall('/v3/depth',{symbol, limit})
 }
 
-module.exports = {time, depth}
+async function exchangeInfo(){
+    return publicCall('/v3/exchangeInfo')
+}
+
+module.exports = {time, depth, exchangeInfo, accountInfo, newOrder}
